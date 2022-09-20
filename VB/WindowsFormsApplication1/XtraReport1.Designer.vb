448c01876b76e8df2ea60289fb52957e815c9dac
DevExpressExampleBot <fakeEmail>
2022-09-20 11:19:45 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/XtraReport1.Designer.vb
+++ VB/WindowsFormsApplication1/XtraReport1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace WindowsFormsApplication1
-    Partial Public Class XtraReport1
+Namespace WindowsFormsApplication1
+
+    Partial Class XtraReport1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,20 +12,20 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Designer generated code"
-
+#Region "Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WindowsFormsApplication1.XtraReport1))
             Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
             Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
             Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
@@ -32,12 +34,12 @@
             Me.xrPictureBox1 = New DevExpress.XtraReports.UI.XRPictureBox()
             Me.xrRichText1 = New DevExpress.XtraReports.UI.XRRichText()
             Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
-            DirectCast(Me.xrRichText1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.xrRichText1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
             ' 
             ' Detail
             ' 
-            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1})
+            Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.xrLabel1})
             Me.Detail.Dpi = 100F
             Me.Detail.HeightF = 36.45833F
             Me.Detail.Name = "Detail"
@@ -71,7 +73,7 @@
             ' 
             ' ReportHeader
             ' 
-            Me.ReportHeader.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2, Me.xrRichText1, Me.xrPictureBox1})
+            Me.ReportHeader.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.xrLabel2, Me.xrRichText1, Me.xrPictureBox1})
             Me.ReportHeader.Dpi = 100F
             Me.ReportHeader.HeightF = 148.9583F
             Me.ReportHeader.Name = "ReportHeader"
@@ -79,7 +81,7 @@
             ' xrPictureBox1
             ' 
             Me.xrPictureBox1.Dpi = 100F
-            Me.xrPictureBox1.Image = (DirectCast(resources.GetObject("xrPictureBox1.Image"), System.Drawing.Image))
+            Me.xrPictureBox1.Image = CType((resources.GetObject("xrPictureBox1.Image")), System.Drawing.Image)
             Me.xrPictureBox1.LocationFloat = New DevExpress.Utils.PointFloat(435.8334F, 10.00001F)
             Me.xrPictureBox1.Name = "xrPictureBox1"
             Me.xrPictureBox1.SizeF = New System.Drawing.SizeF(204.1666F, 138.9583F)
@@ -107,23 +109,28 @@
             ' 
             ' XtraReport1
             ' 
-            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin, Me.ReportHeader})
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.Detail, Me.TopMargin, Me.BottomMargin, Me.ReportHeader})
             Me.ReportPrintOptions.DetailCountOnEmptyDataSource = 55
             Me.Version = "16.2"
-            DirectCast(Me.xrRichText1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
-
+            CType((Me.xrRichText1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
         End Sub
 
-        #End Region
-
+#End Region
         Private Detail As DevExpress.XtraReports.UI.DetailBand
+
         Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+
         Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+
         Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+
         Private ReportHeader As DevExpress.XtraReports.UI.ReportHeaderBand
+
         Private xrPictureBox1 As DevExpress.XtraReports.UI.XRPictureBox
+
         Private xrRichText1 As DevExpress.XtraReports.UI.XRRichText
+
         Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
     End Class
 End Namespace
